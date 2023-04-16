51e75918f30b0aa04010b4e283a2363df41b16ee
GodWin1100 <44332286+GodWin1100@users.noreply.github.com>
2023-04-17 00:02:44 +0530

    feat(settings.py): logger added for request

--- poll_app/poll_app/settings.py
+++ poll_app/poll_app/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from datetime import datetime
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -124,3 +125,24 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "default": {"format": "[%(asctime)s] %(name)s:%(levelname)s::%(funcName)s => %(message)s", "style": "%"}
+    },
+    "handlers": {
+        "file": {
+            "level": "DEBUG",
+            "class": "logging.FileHandler",
+            "filename": f"./logs/DjangoLogger-{datetime.now().strftime('%Y-%m-%dT%H-%M-%S')}.log",
+            "formatter": "default",
+        },
+        "console": {"class": "logging.StreamHandler"},
+    },
+    "loggers": {
+        # "django": {"handlers": ["file", "console"], "level": "INFO", "propagate": True},
+        "django.server": {"handlers": ["file", "console"], "level": "INFO", "propagate": True},
+    },
+}
